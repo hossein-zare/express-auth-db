e1759bf2e5e4f023126986e16a21d4814b179624
Hossein Zare <zarehossein.hz1997@gmail.com>
2022-03-26 12:33:31 +0430

    Update typedefs.

--- index.d.ts
+++ index.d.ts
@@ -3,6 +3,8 @@ import { Request, Response, NextFunction } from 'express';
 export function login(id: any, res: Response): Promise<boolean>;
 export function logout(req: Request, res: Response): Promise<boolean>;
 export function authenticate(req: Request, res: Response, next: NextFunction): Promise<void>;
+export function authenticated(req: Request, res: Response, next: NextFunction, user: any): void;
+export function unauthenticated(req: Request, res: Response, next: NextFunction, clear?: boolean): void;
 export function checkAuthenticated(req: Request, res: Response, next: NextFunction): void;
 export function checkNotAuthenticated(req: Request, res: Response, next: NextFunction): void;
 export function setup(opt: Opt): void;
